839f15c2ed22bcf33c1854e6b97f9dc8cfa2197a
nareshgowd <gowd2009@gmail.com>
2024-06-05 23:15:52 +0530

    adding shell

--- roboshoproject/components/frontend.sh
+++ roboshoproject/components/frontend.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+LOG_FILE=/tmp/roboshop.log
+
 rm -f /tmp/roboshop.log
 
 echo "Installing Nginx"
 
-sudo amazon-linux-extras install nginx1 -y >>/tmp/roboshop.log
+sudo amazon-linux-extras install nginx1 -y >>LOG_FILE
 
+systemctl start nginx
+
+systemctl enable nginx
+systemctl status nginx
 
