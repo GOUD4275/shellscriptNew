c7aa3053b434a2b6389c27c3a94c7ebf8c73f1fa
nareshgowd <gowd2009@gmail.com>
2024-06-05 23:23:52 +0530

    adding shell curl

--- roboshoproject/components/frontend.sh
+++ roboshoproject/components/frontend.sh
@@ -2,14 +2,18 @@
 
 LOG_FILE=/tmp/roboshop.log
 
-rm -f /tmp/roboshop.log
+rm -f /tmp/roboshop.logLOG_FILE
 
 echo "Installing Nginx"
 
-sudo amazon-linux-extras install nginx1 -y >>LOG_FILE
+sudo amazon-linux-extras install nginx1 -y &>>LOG_FILE
 
 systemctl start nginx
 
 systemctl enable nginx
 systemctl status nginx
 
+echo 'clean old content'
+rm -rf /usr/share/nginx/html/* &>>LOG_FILE
+
+curl -s -l -o https://www.free-css.com/assets/files/free-css-templates/download/page296/healet.zip
