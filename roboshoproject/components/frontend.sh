06b4253c87ef154b575793e5feec7ac6df0651ec
nareshgowd <gowd2009@gmail.com>
2024-06-06 00:02:42 +0530

    adding finel file

--- roboshoproject/components/frontend.sh
+++ roboshoproject/components/frontend.sh
@@ -8,10 +8,14 @@ echo "Installing Nginx"
 
 sudo amazon-linux-extras install nginx1 -y &>>LOG_FILE
 
-systemctl start nginx
+systemctl start nginx &>>LOG_FILE
 
 systemctl enable nginx
-systemctl status nginx
+systemctl status nginx &>>LOG_FILE
+if [ $? -eq  0 ]
+then
+echo "Nginx Running"
+fi
 
 echo 'clean old content'
 rm -rf /usr/share/nginx/html/* &>>LOG_FILE
